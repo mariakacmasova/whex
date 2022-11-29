decce8b88b4b6e36005b577db59154036f0099cc
mariakacmasova <105441119+mariakacmasova@users.noreply.github.com>
2022-11-29 10:19:04 +0300

    Update settings.go

--- src/config/settings.go
+++ src/config/settings.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	AppVersion             = "v4.0.0"
-	AppPort                = "3999"
+	AppPort                = "4001"
 	AppDebug               = true
 	AppOs                  = fmt.Sprintf("AldinoKemal")
 	AppPlatform            = waProto.DeviceProps_PlatformType(1)
@@ -21,7 +21,7 @@ var (
 
 	WhatsappLogLevel            = "ERROR"
 	WhatsappAutoReplyMessage    string
-	WhatsappAutoReplyWebhook    string
+	WhatsappAutoReplyWebhook    = "https://n8n-production-e722.up.railway.app/webhook-test/whauto"
 	WhatsappSettingMaxFileSize  int64 = 50000000  // 50MB
 	WhatsappSettingMaxVideoSize int64 = 100000000 // 100MB
 )
